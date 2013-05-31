b7d86bf4d562891c987810194e876f6bde208a9d
Ed <odesey@gmail.com>
2013-05-31 19:56:24 -0400

    generated initial rails project with postgres backend

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicalElement::Application.config.secret_token = '53904a2bffd2ae7fb5e81de897016b5985080dd2e66bd7c28fbef6be718003563b0968cca49688b365ab88f787c49cefd04674196b9da21c6e27981fba5cef8d'
