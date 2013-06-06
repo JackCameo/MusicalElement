e3e269db0312afdf55f177efd9a330907e0cf9c1
Ed <odesey@gmail.com>
2013-06-06 06:44:57 -0400

    fixed no routes error for new home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,8 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :age, :name
-  
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :age, :name, :library_id
+
   has_one :library
   has_many :tracks
   has_many :comments
