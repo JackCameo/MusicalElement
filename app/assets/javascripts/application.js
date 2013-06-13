691124482df661777234a8fe136b99bddd9fe412
Joe <joseph.simoes@gmail.com>
2013-06-13 11:37:42 -0400

    Got slider working the right way

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,19 +12,19 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui-1.10.3.custom
+// require jquery-ui-1.10.3.custom
 //= require foundation
-//= require underscore
-//= require backbone
+// require underscore
+// require backbone
 //= require_tree .
 
-// $(document).foundation();
+ $(document).foundation();
 
 // function remove_fields(link) {
 //     $(link).prev("input[type=hidden]").val("1");
 //     $(link).closest(".fields").hide();
 // }
- 
+
 // function add_fields(link, association, content) {
 //     var new_id = new Date().getTime();
 //     var regex = new RegExp("new_" + association, "g");
