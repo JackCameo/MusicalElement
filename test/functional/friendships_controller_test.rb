32a35b3b43a7acfb4c462734ec514145c0f88b3c
Joe <joseph.simoes@gmail.com>
2013-06-14 09:57:57 -0400

    Picked good slider images

--- test/functional/friendships_controller_test.rb
+++ test/functional/friendships_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
+require 'friendships_controller'
 
-class FriendshipsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class FriendshipsControllerTest < ActionController::Unit::TestCase
+
+  def setup
+    @controller = FriendshipsController.new
+    @request = ActionController::TestRequest.new
+    @response = ActionController::TestResponse.new
+    @user = users(:valid_user)
+  end
+
 end
